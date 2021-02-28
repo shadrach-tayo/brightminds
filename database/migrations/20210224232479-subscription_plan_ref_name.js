a6aa7414351f291514542af08f1196dadc8030cd
shadrach <shadrachtemitayo@gmail.com>
2021-03-01 00:13:06 +0100

    migrate subscription and invoice tables

--- database/migrations/20210224232479-subscription_plan_ref_name.js
+++ database/migrations/20210224232479-subscription_plan_ref_name.js
@@ -2,19 +2,19 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.renameTable('plans', 'plan');
-    await queryInterface.removeColumn('subscription', 'plans_id');
-    await queryInterface.addColumn('subscription', 'plan_id', {
-      type: Sequelize.DataTypes.UUID,
-      references: {
-        model: 'plan',
-        key: 'id',
-      },
-      onUpdate: 'CASCADE',
-      onDelete: 'SET NULL',
-      defaultValue: null,
-      after: 'user_id',
-    });
+    // await queryInterface.renameTable('plans', 'plan');
+    // await queryInterface.removeColumn('subscription', 'plans_id');
+    // await queryInterface.addColumn('subscription', 'plan_id', {
+    //   type: Sequelize.DataTypes.UUID,
+    //   references: {
+    //     model: 'plan',
+    //     key: 'id',
+    //   },
+    //   onUpdate: 'CASCADE',
+    //   onDelete: 'SET NULL',
+    //   defaultValue: null,
+    //   after: 'user_id',
+    // });
   },
 
   down: async (queryInterface, Sequelize) => {
